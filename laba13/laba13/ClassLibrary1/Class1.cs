4f61693d4cad511506a49132851245103c546deb
den669 <71308629+den669@users.noreply.github.com>
2023-04-09 16:30:11 +0300

    Add files via upload

---
+++ laba13/laba13/ClassLibrary1/Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public interface IObserver
+{
+    void Update(string message, string author);
+}
+
+
+public class Chat
+{
+    private List<IObserver> observers = new List<IObserver>();
+
+    
+    public void AddObserver(IObserver observer)
+    {
+        observers.Add(observer);
+    }
+
+    
+    public void RemoveObserver(IObserver observer)
+    {
+        observers.Remove(observer);
+    }
+
+    
+    public void SendMessage(string message, string author)
+    {
+        Console.WriteLine($"{author}: {message}");
+        NotifyObservers(message, author);
+    }
+
+    
+    private void NotifyObservers(string message, string author)
+    {
+        foreach (var observer in observers)
+        {
+            observer.Update(message, author);
+        }
+    }
+}
+
+
+public class ChatParticipant : IObserver
+{
+    private string name;
+
+    public ChatParticipant(string name)
+    {
+        this.name = name;
+    }
+
+    
+    public void Update(string message, string author)
+    {
+        Console.WriteLine($"{name} отримав(ала) повідомлення від {author}: {message}");
+    }
+}
---
+++ laba13/laba13/laba13/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        
+        Chat chat = new Chat();
+
+        
+        ChatParticipant participant1 = new ChatParticipant("Учасник 1");
+        chat.AddObserver(participant1);
+
+        ChatParticipant participant2 = new ChatParticipant("Учасник 2");
+        chat.AddObserver(participant2);
+
+        
+        chat.SendMessage("Привіт всім!", "Автор");
+
+        
+        chat.RemoveObserver(participant2);
+
+        
+        chat.SendMessage("Як справи?", "Автор");
+
+        Console.ReadLine();
+    }
+}
